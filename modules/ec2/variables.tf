2bf6bcc0ed49b265deb5efd0fcc2f1fb93849574
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-02-09 14:39:04 -0400

    feat: create ec2 variables

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,33 @@
+variable "environment" {
+    type        = string
+    description = "Environment name for resource tagging"
+}
+
+variable "vpc_id" {
+    type         = string
+    description  = "ID of the VPC"
+}
+
+variable "instance_type" {
+    type        = string
+    description = "EC2 instance type"
+    default     = "t3.micro"
+}
+
+variable "private_subnet_id" {
+    type        = string
+    description = "ID of the private subnet"
+}
+
+variable "ami_name_filter" {
+    type        = string
+    description = "AMI name filter pattern"
+    default     = "al2023-ami-*-x86_64"
+}
+
+variable "ami_owner" {
+    type        = string
+    description = "AMI owner account ID or alias"
+    default     = "amazon"
+}
+
